d09940787dd2157d966c5187d8f4efb90dc15a16
ILKIN\ifley <i.fleydanli@gmail.com>
2017-11-01 22:05:49 +0400

    several changes

---
+++ app/Http/Controllers/Admin/PageController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Page;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pages = Page::with('translations')->get();
+        return view('backend.page.index', compact('pages'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param $page
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Page $page)
+    {
+        return view('backend.page.form', compact('page'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Page $page
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Page $page)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Page $page
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Page $page)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Page $page
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Page $page)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Page $page
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Page $page)
+    {
+        //
+    }
+}
---
+++ resources/views/backend/page/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.backend')
+
+@section('content')
+    {{ dd($pages) }}
+@endsection
